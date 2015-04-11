79c6892daef69cac6d47d44ff2d685e6e9a47592
Lixquid <Lixquid@users.noreply.github.com>
2015-04-11 01:57:55 +0100

    Added last_seen.js plugin

---
+++ plugins/last_seen.js
@@ -0,0 +1,156 @@
+( function( bot ) {
+
+// Database //
+
+db.run( "CREATE TABLE IF NOT EXISTS last_seen(\
+  user INTEGER PRIMARY KEY, \
+  name TEXT, \
+  time TEXT )" );
+
+// Util //
+
+var minute = 60;
+var hour = 60 * 60;
+var day = 60 * 60 * 24;
+var week = 60 * 60 * 24 * 7;
+var month = 60 * 60 * 24 * 30;
+var year = 60 * 60 * 24 * 365
+
+function plur( num, suffix ) {
+	num = Math.floor( num );
+	return num == 1 ? num + suffix : num + suffix + "s";
+}
+
+function niceTimeSpan( seconds ) {
+	if ( seconds > year )
+		return plur( seconds / year, " year" );
+	else if ( seconds > month )
+		return plur( seconds / month, " month" );
+	else if ( seconds > week )
+		return plur( seconds / week, " week" );
+	else if ( seconds > day )
+		return plur( seconds / day, " day" );
+	else if ( seconds > hour )
+		return plur( seconds / hour, " hour" );
+	else if ( seconds > minute )
+		return plur( seconds / minute, " minute" );
+	else
+		return plur( seconds, " second" );
+}
+
+function getLastSeenMsg( name, new_name, time ) {
+	var msg = new_name + " was last seen ";
+	msg += niceTimeSpan( ( new Date() - new Date( time ) ) / 1000 );
+	msg += " ago";
+	if ( name && name != new_name )
+		msg += " as " + name;
+	msg += ".";
+
+	return msg;
+}
+
+// Event //
+
+bot.on( "UserConnected", function( name, sid ) {
+	db.get( "SELECT name, time FROM last_seen WHERE user = ?",
+	  sid,
+	  function( err, row ) {
+
+		if ( !err && row ) {
+			// Visited Before
+
+			bot.sendMessage( getLastSeenMsg( name, row.name, row.time ) );
+
+			db.run( "UPDATE last_seen \
+			  SET time = ?, name = ? WHERE user = ?",
+			  new Date().toString(), name, sid );
+
+		} else {
+			// New friend!
+
+			bot.sendMessage( "Hi " + name + "! Welcome to the chat!" );
+
+			db.run( "INSERT INTO last_seen \
+			  ( user, name, time ) VALUES ( ?, ?, ? )",
+			  sid, name, new Date().toString() );
+
+		}
+
+	} );
+} );
+
+bot.on( "UserDisconnected", function( name, sid ) {
+
+	db.run( "UPDATE last_seen \
+	  SET time = ?, name = ? WHERE user = ?",
+	  new Date().toString(), name, sid );
+
+} );
+
+// Command //
+
+bot.registerCommand( "lastseen", function( name, steamID, args ) {
+
+	db.all( "SELECT user, name, time FROM last_seen WHERE name LIKE ? \
+	  ORDER BY name LIMIT 11",
+	  "%" + args[0] + "%",
+	  function( err, rows ) {
+
+		if ( err ) {
+			console.error( err );
+			return bot.sendMessage( "Error!" );
+		}
+
+		if ( rows.length == 0 )
+			return bot.sendMessage( "Nobody found with the name '" + args[0]
+			  + "'.");
+
+		var output = "";
+
+		if ( rows.length == 11 ) {
+			output += "More than 10 entries! Some entries have been omitted.\n";
+			rows.pop();
+		}
+
+		// This doesn't work properly for some reason
+		// Client.chatRooms seems to be returning some out of date list
+		// So everyone is displayed as "offline"
+
+		for ( var x = 0; x < 10; x++ )
+			if ( rows[x] != null ) {
+				var row = rows[x];
+				var user = row.user.toString();
+
+				if ( bot.Client.chatRooms[ bot.GroupID ][ user ] != null )
+					output += row.name + " is in the chat right now!";
+				else
+					output += getLastSeenMsg( null, row.name, row.time ) + "\n";
+			}
+
+		bot.sendMessage( output );
+
+	} );
+
+} );
+
+bot.registerCommand( "lastseenid", function( name, steamID, args ) {
+
+	db.get( "SELECT name, time FROM last_seen WHERE user = ?", args[0],
+	  function( err, row ) {
+
+		if ( err ) {
+			console.error( err );
+			return bot.sendMessage( "Error!" );
+		}
+
+		if ( !row )
+			return bot.sendMessage( "Nobody found with the id '" + args[0]
+			  + "'.");
+
+		bot.sendMessage( getLastSeenMsg( name, row.name, row.time ) );
+
+	} );
+
+} );
+
+} )( bot );
