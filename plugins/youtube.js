16cbf8d0dfa55ff747128072281fa8105fd73fe4
William Wallace <me@wiox.me>
2015-05-04 03:32:20 +0100

    YouTube plugin

---
+++ plugins/youtube.js
@@ -0,0 +1,32 @@
+var request     = require('request');
+
+var String_Prototype_Repeat_Is_NonStandard = [
+	"✩✩✩✩✩",
+	"★✩✩✩✩",
+	"★★✩✩✩",
+	"★★★✩✩",
+	"★★★★✩",
+	"★★★★★"
+];
+
+bot.on( "Message", function( name, steamID, msg ) {
+	
+	var match = msg.match( /(youtube\.com\/watch\?v=|youtu\.be\/)([A-Z0-9-_]+)/i );
+
+	if ( !match )
+		return;
+
+	request( "http://gdata.youtube.com/feeds/api/videos/" + match[2] + "?alt=json", function( error, response, body ) {
+
+		if ( error )
+			return; // Fuck Node
+
+		var data = JSON.parse( body );
+		var entry = data.entry;
+
+		var starCount = Math.round( entry.gd$rating.average );
+
+		bot.sendMessage( "YouTube: " + entry.title.$t + " [" + String_Prototype_Repeat_Is_NonStandard[ starCount ] + "]" );
+
+	} );
+} );
