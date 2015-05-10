31c732f0b42ec1961a1a4f5874042a8b01ae108c
William Wallace <me@wiox.me>
2015-05-10 15:21:10 +0100

    Revert "Allow storing fake C functions in cookies"
    This reverts commit 39ede4318def31631aac8e399b0b8c96cd2d7581.

--- plugins/lua/env.lua
+++ plugins/lua/env.lua
@@ -21,10 +21,6 @@ local function ProtectTable( tab, target )
 
 			index[ k ] = ProtectTable( tab[ k ] )
 
-		elseif type( v ) == "function" then
-
-			index[ k ] = function( ... ) return v( ... ) end -- Wrap so that we can store everything in cookies! 
-
 		else
 
 			index[ k ] = tab[ k ]
