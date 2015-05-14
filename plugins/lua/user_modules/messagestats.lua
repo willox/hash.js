0d867b21dd0f3a003920c641b487b47b650108af
MeepDarknessMeep <meepdarknessmeep@yahoo.com>
2015-05-13 22:11:15 -0400

    Update to less shit

--- plugins/lua/user_modules/messagestats.lua
+++ plugins/lua/user_modules/messagestats.lua
@@ -1,8 +1,21 @@
+cookie.texts = cookie.texts or {};
+
+local function add(len)
+	local mean = cookie.texts.mean;
+	local amt = cookie.texts.tracked;
+	
+	mean = (mean * (amt / (amt + 1))) + len / (amt + 1);
+	amt = amt + 1;
+	
+	cookie.texts.tracked = amt;
+	cookie.texts.mean = mean;
+end
+
 hook.Add("Message", "Lengthometer", function(name,id, text) 
-	cookie.texts = cookie.texts or {} 
-	cookie.texts[#cookie.texts + 1] = text:len() 
+	add(text:len());
 end)
-stats = {};
+
+stats = stats or {};
 
 function stats.MeanLength()
 	local max = 0;
