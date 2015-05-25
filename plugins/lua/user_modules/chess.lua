51910d17fc3b68c45f3009d4585c26e467b18f04
Zerf <zerfgog202@gmail.com>
2015-05-25 09:51:59 -0600

    Update chess.lua

--- plugins/lua/user_modules/chess.lua
+++ plugins/lua/user_modules/chess.lua
@@ -1,4 +1,4 @@
-local WHITE, BLACK = 0, 1 -- 1 > 0 :^)
+local WHITE, BLACK = 0, 1
 
 local turn = WHITE
 
@@ -28,6 +28,7 @@ local chars = {
 local board
 
 local function reset()
+	turn = WHITE
 	board = {
 		{BROOK,BKNIGHT,BBISHOP,BQUEEN,BKING,BBISHOP,BKNIGHT,BROOK},
 		{BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN,BPAWN},
@@ -71,13 +72,16 @@ local function xytoxy(xy)
 	return x, y
 end
 
+local function getteam(sid64)
+	return (teams[BLACK] == sid64 and BLACK) or (teams[WHITE] == sid64 and WHITE) or false
+end
+
 local function movepiece(sid64, oldxy, xy)
 	if not (teams[BLACK] and teams[WHITE]) then err("Game not started.") return end
-	if not (teams[BLACK] == sid64 or teams[WHITE] == sid64) then err("You are not in this game.") return end
+	if not getteam(sid64) then err("You are not in this game.") return end
 
-	if teams[turn] ~= sid64 then err("It's not your turn!") return end
+	if turn ~= getteam(sid64) then err("It's not your turn!") return end
 
-	-- get on my level
 	local oldx, oldy = xytoxy(oldxy)
 	if not oldx or oldx > 8 or oldx < 1 then err("Invalid old horizontal position (must be 1-8 or a-h)") return end
 	if not oldy or oldy > 8 or oldy < 1 then err("Invalid old vertical position (must be 1-8)") return end
@@ -88,6 +92,9 @@ local function movepiece(sid64, oldxy, xy)
 	if not x or x > 8 or x < 1 then err("Invalid new horizontal position (must be 1-8 or a-h)") return end
 	if not y or y > 8 or y < 1 then err("Invalid new vertical position (must be 1-8)") return end
 
+	local curspot = board[oldy][oldx]
+	if (getteam(sid64) == BLACK and curspot > 0) or (getteam(sid64) == WHITE and curspot < 0) then err("You may not move the enemy's piece!") return end
+
 	local newspot = board[y][x]
 	if newspot ~= 0 then
 		chessprint(newspot[spot] .. " was captured and removed from play.")
@@ -95,7 +102,7 @@ local function movepiece(sid64, oldxy, xy)
 
 	board[y][x] = board[oldy][oldx]
 
-	board[oldy][oldx] = nil
+	board[oldy][oldx] = 0
 
 	printboard()
 
@@ -115,9 +122,9 @@ hook.Add("Message", "CHESSAGE", function(ply, sid64, msg)
 			teams[BLACK] = sid64
 			chessprint(ply.." joined black side!")
 		end
-		if teams[BLACK] and teams[WHITE] then chessprint("Game starting.") end
+		if teams[BLACK] and teams[WHITE] then chessprint("Game ready.") end
 	elseif subcmd == "leave" then
-		if not (teams[BLACK] == sid64 or teams[WHITE] == sid64) then err("You are not playing.") return end
+		if not getteam(sid64) then err("You are not playing.") return end
 		if teams[WHITE] == sid64 then
 			teams[WHITE] = nil
 			chessprint(ply.." left white side! Game resetting.")
@@ -129,20 +136,19 @@ hook.Add("Message", "CHESSAGE", function(ply, sid64, msg)
 	elseif subcmd == "print" then
 		printboard()
 	elseif subcmd == "status" then
-		chessprint("White player: "..tostring(teams[WHITE]), "Black player: "..tostring(teams[BLACK]) .. "; Turn: "..teamstring[turn])
-	elseif subcmd == "help" then
-		chessprint("Commands: join, leave, print, status, help")
-		chessprint("To move: !chess <piece> <XY>")
+		chessprint("White player: "..tostring(teams[WHITE]), "Black player: "..tostring(teams[BLACK]), "Turn: "..teamstring[turn])
 	elseif subcmd == "reset" then
-		if not (teams[BLACK] == sid64 or teams[WHITE] == sid64) then err("You are not playing.") return end
+		if not getteam(sid64) then err("You are not playing.") return end
 		reset()
+	elseif subcmd == "help" then
+		chessprint("Commands: join, leave, print, status, reset, help", "To move: !chess <old XY> <XY>")
 	else
-		if not (teams[BLACK] == sid64 or teams[WHITE] == sid64) then err("You are not playing.") return end
+		if not getteam(sid64) then err("You are not playing.") return end
 		local space = string.find(subcmd, " ")
-		if not space then return end
-		local piece, xy = string.sub(subcmd, 1, space), string.sub(subcmd, space + 1)
-		if not (piece and xy) then return end
-		movepiece(sid64, piece, xy)
+		if not space then chessprint("To move: !chess <old XY> <XY>") return end
+		local oldxy, xy = string.sub(subcmd, 1, space), string.sub(subcmd, space + 1)
+		if not (oldxy and xy) then chessprint("To move: !chess <old XY> <XY>") return end
+		movepiece(sid64, oldxy, xy)
 	end
 end)
 
