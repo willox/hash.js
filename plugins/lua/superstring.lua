05c1c1be53be34ae0f32f5a2931333150331e5a7
wyozi <wyozi@users.noreply.github.com>
2015-05-27 01:48:48 +0300

    String interpolation

--- plugins/lua/superstring.lua
+++ plugins/lua/superstring.lua
@@ -36,3 +36,13 @@ function meta:__mod( arg )
 	end
 
 end
+
+local function eval_expr( expr )
+	local try_ret, err = load("return " .. expr, "interp", "t", _ENV)
+	if not err then return tostring(try_ret()) end
+	
+	return "{err: " .. err .. "}"
+end
+function meta:__bnot( arg )
+	return string.gsub(arg, "$([^%s]+)", function(var) return eval_expr(var) end)
+end
