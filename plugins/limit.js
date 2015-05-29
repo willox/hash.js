b1d28607831b0bf43a675951cb8485968e65fe36
William Wallace <me@wiox.me>
2015-05-29 06:13:16 +0100

    Limitation system

---
+++ plugins/limit.js
@@ -0,0 +1,36 @@
+db.run( "CREATE TABLE IF NOT EXISTS limited ( user TEXT PRIMARY KEY )", function() {
+
+	db.get( "SELECT user FROM limited", function( err, row ) {
+
+		if ( err )
+			throw err;
+
+		if ( row )
+			bot.setLimited( row.user, true );
+
+	} );
+
+
+} );
+
+bot.registerCommand( "limit", function( name, steamID, _, argstr ) {
+
+	if ( !bot.isAdmin( steamID ) )
+		return;
+
+	bot.setLimited( argstr, true );
+
+	db.run( "INSERT INTO limited VALUES ( ? )", argstr )
+
+} );
+
+bot.registerCommand( "unlimit", function( name, steamID, _, argstr ) {
+
+	if ( !bot.isAdmin( steamID ) )
+		return;
+
+	bot.setLimited( argstr, false );
+
+	db.run( "DELETE FROM VALUES WHERE user = ?", argstr );
+
+} );
