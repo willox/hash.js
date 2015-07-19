94f2363943af265e3257ee8e18b101e7cc9407d3
MeepDarknessMeep <meepdarknessmeep@gmail.com>
2015-07-18 20:44:55 -0400

    added util.CRC

--- plugins/lua/user_modules/gmod_defines.lua
+++ plugins/lua/user_modules/gmod_defines.lua
@@ -244,3 +244,27 @@ function PrintTable( t, indent, done )
 		end
 	end
 end
+
+util = util or {};
+
+function util.CRC(stuff)
+
+	local Polynomial = 0xEDB88320;
+	local lookup = {};
+	for i = 0, 0xFF do
+		local crc = i;
+		for j = 0, 7 do
+			crc = (crc >> 1) ~ ((crc & 1) * Polynomial);
+		end
+		lookup[i] = crc;
+	end
+	local function dw(x) return x & 0xFFFFFFFF; end
+	local poly = 0xEDB88320;
+	local crc = dw(~0);
+	for i = 1, stuff:len() do
+		crc = (crc >> 8) ~ lookup[((crc & 0xFF) ~ stuff:sub(i,i):byte())];
+	end
+	
+	return dw(~crc);
+
+end
