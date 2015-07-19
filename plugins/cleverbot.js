974c410152dc10ddfbeba340f40577a71fbb7edd
William Wallace <me@wiox.me>
2015-07-19 20:08:41 +0100

    Cleverbot

--- plugins/cleverbot.js
+++ plugins/cleverbot.js
@@ -1,4 +1,5 @@
-var cleverbot = new ( require( "cleverbot-node" ) );
+var config = require( "../config.js" );
+var cleverbot = new ( require( "cleverbot" ) )( config.CleverBotUser, config.CleverBotKey );
 var busy = false;
 var excited = false;
 
@@ -8,56 +9,72 @@ var phrases = [
 	"hi"
 ];
 
-// bot.on( "Message", OnMessage ); // STOP POST
+var lastMessage = 0;
 
-function ShouldReply( msg ) {
+cleverbot.setNick( "Hash.JS" );
+cleverbot.create( function( err, session ) {
 
-	// Don't reply if we are already replying
-	if ( busy )
-		return false;
-
-	// 80% chance to reply if the bot's name is spoken.
-	if ( msg.match( /\bHash\b/i ) )
-		return Math.random() > 0.20;
-
-	// 10% chance to reply if excited
-	if ( excited )
-		return Math.random() > 0.90;
-
-	// 10% chance to reply if first word is a defined phrase
-	var firstWordLower = msg.toLowerCase().split( " " )[ 0 ];
-
-	if ( phrases.indexOf( firstWordLower ) != -1 )
-		return Math.random() > 0.9;
-
-	// 1% chance by default
-	return Math.random() > 0.99;
-
-}
-
-function OnMessage( name, steamID, msg, group ) {
-
-	if ( !ShouldReply( msg ) )
+	if ( err )
 		return;
 
-	busy = true;
+	bot.on( "Message", OnMessage );
 
-	// Translate name from Hash to Cleverbot
-	msg = msg.replace( /\bHash\b/ig, "Cleverbot" );
+	function ShouldReply( msg ) {
 
-	cleverbot.write( msg, function( res ) {
+		// Don't reply if we are already replying
+		if ( busy )
+			return false;
 
-		if ( res && res.message ) {
+		// 0.5 messages per minute
+		if ( lastMessage + 120 > Date.now() )
+			return false;
+		
+		// 80% chance to reply if the bot's name is spoken.
+		if ( msg.match( /\bHash\b/i ) )
+			return Math.random() > 0.20;
+
+		// 10% chance to reply if excited
+		if ( excited )
+			return Math.random() > 0.90;
+
+		// 10% chance to reply if first word is a defined phrase
+		var firstWordLower = msg.toLowerCase().split( " " )[ 0 ];
+
+		if ( phrases.indexOf( firstWordLower ) != -1 )
+			return Math.random() > 0.9;
+
+		// 1% chance by default
+		return Math.random() > 0.99;
+
+	}
+
+	function OnMessage( name, steamID, msg, group ) {
+
+		if ( !ShouldReply( msg ) )
+			return;
+
+		busy = true;
+
+		// Translate name from Hash to Cleverbot
+		msg = msg.replace( /\bHash\b/ig, "Cleverbot" );
+
+		lastMessage = Date.now();
+
+		cleverbot.ask( msg, function( err, res ) {
+
+			if ( err )
+				return;
 
 			// Translate name from Cleverbot to Hash
-			res.message = res.message.replace( /\bCleverbot\b/ig, "Hash" );
+			res = res.replace( /\bCleverbot\b/ig, "Hash" );
 
-			bot.sendMessage( res.message, group );
+			bot.sendMessage( res, group );
 
-		}
+			busy = false;
 
-		busy = false;
+		} );
 
-	} );
+	}
 
-}
+
+} );
