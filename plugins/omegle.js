34815a6a89ababd59170d063400cafad916071a6
William Wallace <me@wiox.me>
2015-09-19 03:36:46 +0100

    Revert "Update omegle plugin"

--- plugins/omegle.js
+++ plugins/omegle.js
@@ -1,232 +1,65 @@
-var client = require( "../lib/omegle" ).Client;
+var omegle = new ( require( "../lib/omegle" ).Client );
 
-var clients = {};
+var connected = false;
 
-function Connect(which, tags) {
+function Connect(tags) {
 
-	bot.sendMessage( "Connecting to Omegle '" + which.omegleid + "'" );
+	bot.sendMessage( "Connecting to Omegle." );
 
-	which.connecting = true;
-
-	which.start( tags, function( e ) {
+	omegle.start(tags, function( e ) {
 
 		if ( e )
 			return;
 
+		connected = true;
+
 	} );
 
 }
 
-function Disconnect(which) {
+function Disconnect() {
 
-	if ( !(which.connected || which.connecting) ) 
-		return;
-	bot.sendMessage( "Disconnected from Omegle '" + which.omegleid + "'" );
+	bot.sendMessage( "Disconnected from Omegle." );
 
-	which.disconnect();
-
-	which.connected = false;
-	which.connecting = false;
+	omegle.disconnect();
+	connected = false;
 
 }
 
-function CreateNewOmegle(id)
-{
-	
-	var which = new client();
-	
-	which.omegleid = id;
-	which.queue = [];
+omegle.on( "connected", function() {
 
-	which.on( "connected", function() {
-		
-	
-		this.connected = true;
-		this.connecting = false;
-
-		bot.sendMessage( "Stranger connected on '" + this.omegleid + "'" );
-		
-		for ( var k in clients ) 
-			if ( k != this.omegleid && (this.mitm && this.mitm == clients[k].mitm || true) ) 
-			{
-				if ( clients[k].queue )
-				{
-					
-					var q;
-					while ( q = clients[k].queue.shift() )
-						this.send ( q );
-					
-				}
-			}
-
-	} );
-	
-	which.on( "event", function(event)
-	{
-		
-		console.log(event);
-		
-	});
-
-	which.on( "gotMessage", function( msg ) {
-
-		bot.sendMessage( ( this.mitm ? "(MITM #" + this.mitm + ") " : "" ) + "Stranger " + this.omegleid + ": " + msg );
-		
-		for ( var k in clients ) 
-			if ( k != this.omegleid && (this.mitm && this.mitm == clients[k].mitm || true) ) 
-			{
-				if ( clients[k].connected )
-				{
-					clients[k].send ( msg );
-				}
-				else 
-				{
-					this.queue.push(msg);
-				}
-			}
-	} );
-
-	which.on( "strangerDisconnected", function() {
-		
-		for ( var k in clients )
-			if( this.mitm && clients[k].mitm && clients[k].mitm == this.mitm )
-				Disconnect ( clients[k] );
-		
-		Disconnect(this);
-
-	} );
-	
-	return which;
-	
-}
-
-bot.on( "Message", function( name, steamID, msg ) {
-
-	for(var k in clients)
-		if ( clients[k].connected && !clients[k].mitm ) 
-			clients[k].send( name + ": " + msg );
+	bot.sendMessage( "Stranger connected." );
 
 } );
 
-var allowed_keys = {
-	
-	'mitm': true,
-	'proxy_port': true,
-	'proxy_ip': true,
-	
-}
+omegle.on( "gotMessage", function( msg ) {
+
+	bot.sendMessage( "Stranger: " + msg );
+
+} );
+
+omegle.on( "strangerDisconnected", function() {
+
+	bot.sendMessage( "Stranger disconnected." );
+	Disconnect();
+
+} );
+
+bot.on( "Message", function( name, steamID, msg ) {
+
+	if ( connected )
+		omegle.send( name + ": " + msg );
+
+} );
 
 bot.registerCommand( "omegle", function( name, steamID, args ) {
 
 	if ( !bot.isAdmin( steamID ) )
 		return;
-		
-	if ( args.length < 2 )
-		return;
-		
-	var command = args.shift();
-	
-	var id = args.shift();
-	
-	switch(command)
-	{
-		
-		case "create":
-		
-			if(id == "all")
-			{
-				bot.sendMessage("Not allowed!");
-				break;
-			}
-		
-			clients[id] = CreateNewOmegle(id);
-			
-			bot.sendMessage( "Created omegle " + id );
-			
-			break;
-			
-		case "reset":
-			
-			if ( id == "all" )
-				for ( var k in clients )
-				{
-					
-					Disconnect(clients[k]);
-					
-					clients[k] = null;
-					
-				}
-			else if ( clients[id] )
-			{
-				Disconnect( clients[id] );
-				clients[id] = null;
-			}
-			break;
-			
-		case "start":
-			if ( id == "all" )
-				for ( var k in clients )
-				{
-					
-					Connect ( clients[k], args );
-					
-				}
-				
-			else if ( clients[id] )
-				Connect ( clients[id], args );
-				
-			break;
-			
-		case "stop":
-		
-			if ( id == "all" )
-				for ( var k in clients )
-				{
-					
-					Disconnect ( clients[k] );
-					
-				}
-			
-			else if ( clients[id] )
-				Disconnect ( clients[id] );
-			
-			break;
-			
-		case "set":
-			
-			if(args.length < 2) break;
-			
-			var key = args.shift();
-			
-			if(!allowed_keys[key])
-			{
-				
-				bot.sendMessage("Not allowed.");
-				
-				break;
-				
-			}
-				
-			var value = args.shift();
-			
-			if ( id == "all" )
-				for ( var k in clients )
-				{
-					
-					clients[k][key] = value;
-					
-				}
-			
-			else if ( clients[id] )
-				clients[id][key] = value;
-				
-			break;
-			
-		default:
-		
-			bot.sendMessage("Unknown subcommand.");
-			break;
-			
-	}
+
+	if ( connected )
+		Disconnect();
+	else
+		Connect(args);
 
 }, "[ADMIN] Connect the group chat to a random omegle user." );
