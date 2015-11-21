db6a8378fee35d59256913b35507fbf3fb5fc16f
Ott <daaott@gmail.com>
2015-11-20 21:23:01 -0800

    memespeak

---
+++ plugins/lua/user_modules/memespeak.lua
@@ -0,0 +1,39 @@
+local emotes = {
+	[" "] = "  ",
+	a = ":csgoa:",
+	b = ":csgob:",
+	c = ":carbon:",
+	d = ":thed:",
+	e = ":elementsigma:",
+	f = ":fluorine:",
+	g = ":gmod:",
+	h = ":hyperion:",
+	i = ":patrol:",
+	j = ":journeyj:",
+	k = ":civilwarsoldier:",
+	l = ":collectible:",
+	m = ":corporatebranding:",
+	n = ":Teddy_Exit:",
+	o = ":chopyouup:",
+	p = ":P:",
+	q = ":hypercube:",
+	r = ":memory:",
+	s = ":cowcredits:",
+	t = ":knightcross:",
+	u = ":GoldHorseShoe:",
+	v = ":flying:",
+	w = ":warband:",
+	x = ":tinyjigsaw:",
+	y = ":Y:",
+	z = ":Z:",
+}
+hook.Add("Message", "memespeak", function(ply, id, txt)
+	if string.sub(txt, 1, 1) == ">" then
+		txt = string.sub(txt, 2)
+		txt = string.lower(txt)
+		for char, emote in pairs(emotes) do
+			txt = string.gsub(txt, char, emote)
+		end
+		print(txt)
+	end
+end)
