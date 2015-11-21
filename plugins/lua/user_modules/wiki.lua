e45478901182a2911a76eb301fb0140800dc1ca0
William Wallace <me@wiox.me>
2015-11-21 02:59:51 +0000

    Create wiki.lua

---
+++ plugins/lua/user_modules/wiki.lua
@@ -0,0 +1,24 @@
+-- 
+
+hook.Add( "Message", "WikI!", function( _, _, msg )
+    local searchTerm = msg:match( "^what is (.*)%??" )
+    
+    if not searchTerm then
+      return
+    end
+    
+    searchTerm = searchTerm:gsub( " ", "%%20" )
+    
+    http.Fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=true&exchars=256&titles=" .. searchTerm,
+      function(c, b)
+        pcall( function()
+          local ex = select( 2, next( json.decode( b ) . query . pages ) ) . extract
+          
+          if not ex or #ex < 5 then
+            return
+          end
+          
+          print( ex )
+        end )
+      end )
+end )
