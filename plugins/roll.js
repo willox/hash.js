ec720d2a51980df9163af54653d1d81b8e70b7e8
unknown <spencer@Spencer-PC.sharkey.pw>
2016-01-12 00:40:33 -0500

    removed useless randInt function and made sense of the math.

--- plugins/roll.js
+++ plugins/roll.js
@@ -1,12 +1,7 @@
-// Returns a random integer between 1 and max
-function randInt( max ) {
-	return Math.floor( Math.random() * ( max - 1 ) ) + 1;
-}
-
 bot.registerCommand( "roll", function( name, _, _, arg_str, group ) {
 
 	if ( /(\d+)?d\d+/.test( arg_str ) ) {
-
+		
 		var match = arg_str.match( /(\d+)?d(\d+)/ );
 		var num = match[1]
 		  ? Math.max( Math.min( parseInt( match[1] ), 10 ), 1 )
@@ -15,7 +10,7 @@ bot.registerCommand( "roll", function( name, _, _, arg_str, group ) {
 
 		var results = [];
 		for ( var x = 0; x < num; x++ )
-			results[ x ] = randInt( max + 1 );
+			results[ x ] = Math.floor( Math.random() * max ) + 1;
 
 		var msg = name + " rolls ";
 		msg += num == 1 ? "a" : num;
