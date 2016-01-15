7edfb3d67f0af5f9ba77a2d1b00c8bfbb89e99e4
Spencer Sharkey <spencer@sf-n.com>
2016-01-15 10:33:51 -0500

    Indentation OCD

--- plugins/roll.js
+++ plugins/roll.js
@@ -1,7 +1,6 @@
 bot.registerCommand( "roll", function( name, _, _, arg_str, group ) {
 
 	if ( /(\d+)?d\d+/.test( arg_str ) ) {
-		
 		var match = arg_str.match( /(\d+)?d(\d+)/ );
 		var num = match[1]
 		  ? Math.max( Math.min( parseInt( match[1] ), 10 ), 1 )
@@ -19,7 +18,6 @@ bot.registerCommand( "roll", function( name, _, _, arg_str, group ) {
 		msg += ": " + results.join( ", " );
 
 		bot.sendMessage( msg, group );
-
 	} else {
 		// Just roll d20
 		bot.sendMessage( name + " rolls a d20: " + randInt( 20 ), group )
