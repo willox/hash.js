9c002c9659ec2f07edc8f6baefd6db6bbbf20019
mcd1992 <theanonbutinf@gmail.com>
2016-02-02 17:28:43 -0600

    Re-added randInt function to roll.js

--- plugins/roll.js
+++ plugins/roll.js
@@ -1,15 +1,19 @@
+function randInt( max ) {
+    return Math.floor( Math.random() * (max || 1) ) + 1;
+}
+
 bot.registerCommand( "roll", function( name, _, _, arg_str, group ) {
 
 	if ( /(\d+)?d\d+/.test( arg_str ) ) {
 		var match = arg_str.match( /(\d+)?d(\d+)/ );
 		var num = match[1]
-		  ? Math.max( Math.min( parseInt( match[1] ), 10 ), 1 )
+		  ? Math.max( Math.min( parseInt( match[1] ), 10 ), 1 ) // Clamp number of die to 10
 		  : 1;
 		var max = parseInt( match[2] );
 
 		var results = [];
-		for ( var x = 0; x < num; x++ )
-			results[ x ] = Math.floor( Math.random() * max ) + 1;
+		for ( var x = 0; x < num; x++ ) // Roll each die and store result
+			results[ x ] = randInt( max );
 
 		var msg = name + " rolls ";
 		msg += num == 1 ? "a" : num;
